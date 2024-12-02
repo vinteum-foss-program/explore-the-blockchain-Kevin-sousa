16d5118400da4ef4f5459ba8557a47310d649d77
Kevin-sousa <52057024+Kevin-sousa@users.noreply.github.com>
2024-12-02 20:52:11 -0300

    003.sh

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,16 @@
 # How many new outputs were created by block 123,456?
+#!/bin/bash
+BLOCK_NUMBER=123456
+
+BLOCK_HASH=$(bitcoin-cli getblockhash $BLOCK_NUMBER)
+
+TXIDS=$(bitcoin-cli getblock $BLOCK_HASH | jq -r '.tx[]')
+
+TOTAL_OUTPUTS=0
+
+for TXID in $TXIDS; do
+  OUTPUT_COUNT=$(bitcoin-cli getrawtransaction $TXID true | jq '.vout | length')
+  TOTAL_OUTPUTS=$((TOTAL_OUTPUTS + OUTPUT_COUNT))
+done
+
+echo "Total de outputs criados no bloco $BLOCK_NUMBER: $TOTAL_OUTPUTS"
